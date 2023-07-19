6cd5ca62ce09f478cb39abc61646790edbe81193
Daniela <Daniela@DESKTOP-P4BHM2A>
2023-07-19 22:57:47 +0100

    Adicionar arquivos de projeto.

---
+++ Assembly1/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Assembly1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Qual é o teu primeiro nome? ");
+
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Qual é o teu segundo nome? ");
+            string middleName = Console.ReadLine();
+
+            Console.WriteLine("Qual é o teu último nome? ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Qual é o teu nome completo? ");
+            string fullName = Console.ReadLine();
+
+            Console.WriteLine("Qual é o teu gênero? ");
+            string genero = Console.ReadLine();
+
+            Console.WriteLine("Qual é a sua idade? ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual é o seu ano de nascimento? ");
+            int nasc = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual é seu trabalho atual? ");
+            string trabalho = Console.ReadLine();
+
+
+            Console.WriteLine("Qual é sua altura? ");
+            double altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a cor dos seus olhos? ");
+            string olho = Console.ReadLine();
+
+            Console.WriteLine($"My full name is {firstName} {middleName} {lastName}");
+
+            DateTime dataAtual = DateTime.Now;
+
+            int idade = DateTime.Now.Year - nasc;
+
+            Console.WriteLine($"A sua idade é: {idade}");
+
+
+
+        }
+    }
+}
